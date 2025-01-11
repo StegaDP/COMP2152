2ce86da412cd715eb60dbb8b846eb7fe723ad6ce
StegaDP <stegailodp@gmail.com>
2025-01-11 17:57:29 -0500

    testing auth

--- Week 01/Lab/Coding Questions/lab1.py
+++ Week 01/Lab/Coding Questions/lab1.py
@@ -1,17 +1,17 @@
 # Sample Coding Questions 01 Week 01
 # Dmitrii Stegailo 101483865
 
-# Defining Variables
+# 1. Defining Variables
 array = [1, 4, 7, 9]
 
-# Order of Operations
+# 2. Order of Operations
 a, b, c, d = 1,2,3,4
 e = a - ((b**c) // d) + (a % c)
 
-# Formating
+# 3. Formating
 temperature = 32.6
 print(f'The temperature today is: {temperature:0.3f} degrees Celsius')
 
-# Common Functions
+# 4. Common Functions
 userAge = int(input('Enter your age: '))
 print(f"Now showing the shop items filtered by age: {userAge}")
